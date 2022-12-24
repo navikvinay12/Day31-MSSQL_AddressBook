8ccc2f416894cd0a5cba53a6ae8d61e32835bea1
Vinay <navik.vinay12@gmail.com>
2022-12-24 17:44:08 +0530

    UC2 creating table in db

--- AddressBookProblem.sql
+++ AddressBookProblem.sql
@@ -2,3 +2,17 @@
 
 --UC1 Creating a new Database named AddressBookService--
 create database AddressBookService;
+use AddressBookService;
+
+--UC2 Creating table named AddressBook--
+create table AddressBook
+(
+firstName varchar(200),
+lastName varchar(200),
+address varchar(200),
+city varchar (200),
+state varchar (200),
+zip int ,
+phoneNumber bigint,
+email varchar (200) 
+);
