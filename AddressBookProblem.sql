4cea0fc236abed4831eade8e926c3e57e518e5fa
Vinay <navik.vinay12@gmail.com>
2022-12-24 17:45:21 +0530

    UC3 -insertion into table

--- AddressBookProblem.sql
+++ AddressBookProblem.sql
@@ -16,3 +16,12 @@ zip int ,
 phoneNumber bigint,
 email varchar (200) 
 );
+
+--UC3 Insert new Contact to AddressBook Table--
+insert into AddressBook values('Vicky','Kaushal','Bandra','Mumbai','Maharashtra',400051,9987465744,'VickyKaushal@gmail.com');
+insert into AddressBook values('Arun','Singh','konkani','Panaji','Goa',510062,9387465756,'ArunSingh@gmail.com');
+insert into AddressBook values('Raj','Kapoor','Raipur ','Dehradun','Uttarakhand',230073,9687565734,'RajKapoor@gmail.com');
+insert into AddressBook values('Sanjay','Thakkur','Nepali','Gangtok','Sikkim',700084,9657465722,'SanjayThakkur@gmail.com');
+insert into AddressBook values('Akshay','jadhav','Oriya','Bhubaneswar','Odisha',600095,9117465741,'Akshayjadhav@gmail.com');
+insert into AddressBook values('Vishal','pal','Umiam Lake','Shillong','Meghalaya',800011,8887465777,'Vishalpal@gmail.com');
+insert into AddressBook values('Vijaya','laxmi','Umiam Lake','Shillong','Meghalaya',800011,8887465777,'Vijayalaxmi@gmail.com');
